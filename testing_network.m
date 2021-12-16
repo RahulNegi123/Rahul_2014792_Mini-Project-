97dd59f3d8aa3938d33ef8e3298ea90fe86add9d
unknown <pentiumrahul2001@gmail.com>
2021-12-16 20:20:18 +0530

    Committing the project

---
+++ testing_network.m
@@ -0,0 +1,8 @@
+function testing_network(net, image)
+I = imread(image);
+G = imresize(I, [224, 224]);
+[Label, Prob] = classify(net,G);
+figure;
+imshow(G);
+title({char(Label), num2str(max(Prob),2)});
+end
